e3d6e09bb845bc0bd5999f138095a422ffac3a0a
TheBritKnight <charles.dwsn@gmail.com>
2013-11-08 15:33:47 -0500

    Changes from meeting on 11/1/13

--- SerialMotorControl/DocksideController/DocksideController.pde
+++ SerialMotorControl/DocksideController/DocksideController.pde
@@ -11,21 +11,31 @@ Serial port;
 ControllIO controllIO;
 ControllDevice joypad;
 ControllStick joy1;
+ControllStick joy2;
 
 int joy1x = 0;
+int joy1y = 0;
+int joy2x = 0; 
+int joy2y = 0;
 
 void setup(){
   size(600, 600);
   noStroke();
   textSize(32);
   
+  
   //Set up controller
   controllIO = ControllIO.getInstance(this);
   joypad = controllIO.getDevice("Afterglow Gamepad for Xbox 360");
  
-  //Set up joystick w/ proper multiplier (negative because otehrwise it's inverted)
+  //Set up joystick w/ proper multiplier for x and y values (negative because otehrwise it's inverted)
   joy1 = joypad.getStick(0);
   joy1.setMultiplier(-255);
+ 
+  
+  //Set Up 2nd joystick w/ proper multiplier for x and y values (see above for why)
+  joy2 = joypad.getStick(1); 
+  joy2.setMultiplier(-255); 
   
   //Set up serial
   println(Serial.list());
@@ -35,11 +45,23 @@ void setup(){
 void draw() {
   background(0.5);
   
-  joy1x = (int) joy1.getX();
+  joy1y = (int) joy1.getX();
+  joy1x = -(int) joy1.getY(); //y-values need to be inverted, because the controller is weird like that, as I believe I has already noted
+  joy2y = (int) joy2.getX();
+  joy2x = -(int) joy2.getY(); 
+
   
-  text("x: " + str(joy1x), 50, 50);
-  port.write(str(joy1x));
+  text("x joystick 1: " + str(joy1x), 50, 50);
+  text("y joystick 1: " + str(joy1y), 50, 100);
+  text("x joystick 2: " + str(joy2x), 50, 150); 
+  text("y joystick 2: " + str(joy2y), 50, 200);
   
+  String toSend = "";
+  toSend += str(joy1x) + "/";
+  toSend += str(joy1y) + "/";
+  toSend += str(joy2x) + "/";
+  toSend += str(joy2y) + "/";
+  port.write(toSend);
   delay(10);
 }
 
--- SerialMotorControl/SerialMotorControl.ino
+++ SerialMotorControl/SerialMotorControl.ino
@@ -1,50 +1,104 @@
-int a = 0; // for incoming serial data
-int PWMpin = 9; // for outputing PWM signal
-int directionPin = 3; // Written high if input >= 0, low otherwise
+#define NUM_MOTORS 4
+#define NUM_BUTTONS 0
+
+int motorPins[NUM_MOTORS] = {9, 3, 5, 6};
+int motorPower[NUM_MOTORS];
+int motorDirection[NUM_MOTORS];
+int buttonStates[NUM_BUTTONS];
 
 void setup() {
   //Set up serial
   Serial.begin(9600);  //  opens serial port 
   
   //Set up pins
-  pinMode(PWMpin, OUTPUT);
-  pinMode(directionPin, OUTPUT);
+  motorPins[0] = 9;
+  motorPins[1] = 3;
+  motorPins[2] = 5;
+  motorPins[3] = 6;
+  //motorPins[4] = 10;
+  //motorPins[5] = 11;
+    
+  //Set up pins and arrays
+  for (int n = 0; n < NUM_MOTORS; n++) {
+    pinMode(motorPins[n], OUTPUT);
+    motorPower[n] = 0;
+  }
   
-  //Initialize output to off
-  a = 0;
  }
 
 void loop () {
   
-  if ( Serial.available() > 0 ) {
-    a = atoi( readCharArrayFromSerial() );
+  readCharArrayFromSerial();
+  
+  for ( int i = 0; i < NUM_MOTORS; i++) {
+    analogWrite( motorPins[i], abs(motorPower[i]) );
+    
+    /*
+    if ( motorPower[i] >= 0 ) {
+      digitalWrite( motorDirection[i], HIGH);
+    } else {
+      digitalWrite( motorDirection[i], LOW);
+    }
   }
-  
-  analogWrite( PWMpin, abs(a) );
-  
-  if ( a >= 0 ) { digitalWrite( directionPin, HIGH); }
-  else { digitalWrite( directionPin, LOW); }
-  
+  for ( int i = 0; i < NUM_BUTTONS; i++) {
+  }
+  */
+  }
   delay(10);
 }
 
 //Utility method to read a string (in the form of a char array) from Serial
-char* readCharArrayFromSerial() {
-  String input = "";
-  int delimiter = (int)'/n';
+void readCharArrayFromSerial() {
+  int motorDelimiter = (int)'/';
+  int buttonDelimiter = (int)'\\';
   
-  while ( Serial.available() > 0 ) {
+  //Scan for motor values first
+  int index = 0;
+  String input = "";
+  
+  while ( Serial.available() > 0 && index < 4 ) {
     int c = Serial.read();
     
-    if ( c == delimiter ) { break; }
     
-    input += (char) c;
+    //Handle moving on to the next value if motor delimeter is recieved
+    if ( c == motorDelimiter ) {
+      //Copy string into char buffer
+      int motorInputLength = input.length() + 1; // + 1 to make room for null terminator
+      char motorInputBuffer[motorInputLength];
+      input.toCharArray(motorInputBuffer, motorInputLength);
+      motorPower[index] = atoi(motorInputBuffer);
+      
+      index ++;
+      input = "";
+    } else {
+      
+      //Handle other delimeters
+      if ( c == buttonDelimiter) { break; }
+    
+      //If no delimeter found, add the char to the input string
+      input += (char) c;
+    }
   }
   
-  //Copy string into char buffer
-  int inputLength = input.length() + 1; // + 1 to make room for null terminator
-  char inputBuffer[inputLength];
-  input.toCharArray(inputBuffer, inputLength);
-  
-  return inputBuffer;
+  /*
+  //Then scan for button values
+  index = 0;
+  input = "";
+  while ( Serial.available() > 0 && index < 0) {
+    int b = Serial.read();
+    
+    //Handle button delimeters
+    if ( b == buttonDelimiter ) {
+      int buttonInputLength = input.length() + 1;
+      char buttonInputBuffer[buttonInputLength];
+      input.toCharArray(buttonInputBuffer, buttonInputLength);
+      buttonStates[index] = atoi(buttonInputBuffer);
+      
+      index ++;
+    } else {
+      input += (char) b;
+    }
+    
+  }
+  */
 }
