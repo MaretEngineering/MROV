5c9e281e0b393fee7a740d90bba0f5f1010ad668
Eclip5e5 <eseide@maret.org>
2014-01-24 23:18:40 -0500

    NewNewNew Com Protocol

--- Communication__NewHARDWAY/Communication__NewHARDWAY.ino
+++ Communication__NewHARDWAY/Communication__NewHARDWAY.ino
@@ -3,49 +3,69 @@
 
 /* !motor/motor/{Button|Button|$ */
 
+// !XXX/XXX/XXX/XXX/XXX/{X|X|X|X|$
+int led = 10;
 
 void setup() {
   Serial.begin(9600);
+  //pinMode(led, OUTPUT);
 }
 
 void loop() {
-  String input = read_input(31);
+  String input;
+  input = read_input();
+  motorValue(input, 2);
+  //analogWrite(led, motorValue(input, 1));
+  //delay(500);
+  
 }
 
-char* read_input (int max_length) {
+String read_input () {
   int index = 0;
-  char data[max_length];
+  char data[31];
   
   while (Serial.available() <= 0 || Serial.read() != '!') {
- }
- while (Serial.available() > 0 && index < max_length) {
+  }
+ while (Serial.available() > 0) {
    char input = Serial.read();
-   if (input == '$') {break; }
+   Serial.println(input);
+   if (input == '$') {Serial.println("End of string"); break; }
    data[index] = input;
    index ++;
  }
- return data;
+ Serial.println(data);
+ return String(data);
 }
 
-int* motorValue(char inputData[], int whichMotor) {
-  placeHolder = 0;
+
+int motorValue(String inputData, int motorNumber) {
+  Serial.println("Hello");
+  int placeHolder = 0;
   
-  if (whichMotor == 1) {
-    placeHolder = 1;
+  switch (motorNumber) {
+    case 1:
+      placeHolder = 1;
+      break;
+    case 2:
+      placeHolder = 5;
+      break;
+    case 3:
+      placeHolder = 9;
+      break;
+    case 4:
+      placeHolder = 13;
+      break;
+    case 5:
+      placeHolder = 17;
+      break;
   }
-  else if (whichMotor == 2) {
-    placeHolder = 5;
-  }
-  else if (whichMotor == 3) {
-    placeHolder = 9;
-  }
-  else if (whichMotor == 4) {
-    placeHolder = 13;
-  }
-  else if (whichMotor == 5) {
-    placeHolder = 17;
-  }
-  return int motorValue = atoi(inputData.substring(placeholder, placeholder+3)
-  
+  Serial.println(placeHolder); //debugging
+  Serial.println(inputData);
+  String val = inputData.substring(0, 3);
+  Serial.println(val); //debugging
+  return val.toInt();
+  //Serial.println(val.toInt());
+}
+
    
    
