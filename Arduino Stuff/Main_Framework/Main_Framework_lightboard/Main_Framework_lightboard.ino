8c30beda79f5a9a19d5a36bbb450bddf385415b6
Eclip5e5 <eseide@maret.org>
2014-02-11 10:21:59 -0500

    Main framework light board
    main framework without the buttons and servos

---
+++ Arduino Stuff/Main_Framework/Main_Framework_lightboard/Main_Framework_lightboard.ino
@@ -0,0 +1,150 @@
+#define FORWARD HIGH
+#define BACKWARD LOW
+
+//Variables!
+double motorValues[] = {0, 0, 0, 0, 0};
+
+//Motor Pins
+#define motor1Pin 0
+#define motor2Pin 0
+#define motor3Pin 0
+#define motor4Pin 0
+#define motorUp1Pin 0
+#define motorUp2Pin 0
+#define motor1PinDir 0
+#define motor2PinDir 0
+#define motor3PinDir 0
+#define motor4PinDir 0
+#define motorUp1PinDir 0
+#define motorUp2PinDir 0
+int motorValuePins[] = {motor1Pin, motor2Pin, motor3Pin, motor4Pin, motorUp1Pin, motorUp2Pin};
+int motorDirPins[] = {motor1PinDir, motor2PinDir, motor3PinDir, motor4PinDir, motorUp1PinDir, motorUp2PinDir};
+
+String inputData = "";
+
+void setup() {
+  
+  initSerial();
+  
+  for (int i = 0; i < 6; i++) {
+    pinMode(motorValuePins[i], OUTPUT);
+    pinMode(motorDirPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  recievePacket();
+  parseThePacket();
+//actOnDepthValues();
+  act();
+}
+
+void initSerial() {
+  // Initialize the serial
+  Serial.begin(115200);
+}
+
+String recievePacket() {
+  int index = 0;
+  char data[32];
+  
+  while (Serial.available() <= 0 || Serial.read() != '!') {}
+  delay(1);
+  while (Serial.available() > 0) {
+    char input = Serial.read();
+    if (input == '$') { break; }
+    data[index] = input;
+    index ++;
+    delay(1);
+ }
+ data[index] = '\0';
+ inputData = String(data);
+}
+
+void parseThePacket() {
+  parseMotorValues();
+
+}
+
+void parseMotorValues() {
+  for (int i = 1; i <= 5; i++) {
+    motorValues[i-1] = getMotorValueAt(i);
+  }
+}
+
+int getMotorValueAt(int motorNumber) {
+  int placeHolder = 0;
+  
+  switch (motorNumber) {
+    case 1:
+      placeHolder = 0;
+      break;
+    case 2:
+      placeHolder = 4;
+      break;
+    case 3:
+      placeHolder = 8;
+      break;
+    case 4:
+      placeHolder = 12;
+      break;
+    case 5:
+      placeHolder = 16;
+      break;
+  }
+  String val = inputData.substring(placeHolder, placeHolder + 3);
+  return val.toInt();
+}
+
+void act() {
+  // Act
+  actOnMotors();
+  
+}
+
+void actOnMotors() {
+  //Deal with horizontal motors
+  for (int i = 0; i < 4; i++) {
+    int a = motorValues[i];
+    a -= 256;
+    if (a < 0) {
+      a = abs(a);
+      a = constrain(a, 80, 256);
+      analogWrite(motorValuePins[i], a);
+      digitalWrite(motorDirPins[i], BACKWARD);
+    } else {
+      a = abs(a);
+      a = constrain(a, 80, 256);
+      analogWrite(motorValuePins[i], a);
+      digitalWrite(motorDirPins[i], FORWARD);
+    }
+  }
+  
+  //Now deal with those pesky depth motors
+  int a = motorValues[4];
+  a -= 256;
+  if (a < 0) {
+    a = abs(a);
+    a = constrain(a, 80, 256);
+    analogWrite(motorValuePins[4], a);
+    digitalWrite(motorDirPins[4], BACKWARD);
+    analogWrite(motorValuePins[5], a);
+    digitalWrite(motorDirPins[5], BACKWARD);
+  } else {
+    a = abs(a);
+    a = constrain(a, 80, 256);
+    analogWrite(motorValuePins[4], a);
+    digitalWrite(motorDirPins[4], FORWARD);
+    analogWrite(motorValuePins[5], a);
+    digitalWrite(motorDirPins[5], FORWARD);
+  }
+}
+
+
+
+
+
+
+
+
+
