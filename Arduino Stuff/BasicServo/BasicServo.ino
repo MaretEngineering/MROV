7ff3c87c1d47fac567ee9c14493dc20dc66b676f
TheBritKnight <charles.dwsn@gmail.com>
2014-02-21 17:10:44 -0500

    Pre sync commit

--- Arduino Stuff/BasicServo/BasicServo.ino
+++ Arduino Stuff/BasicServo/BasicServo.ino
@@ -2,20 +2,20 @@
 
 #define SERVO_1_PIN 10
 
-Servo servo1
+Servo servo1;
 
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(9600);
   servo1.attach(SERVO_1_PIN);
   
 }
 
 void loop() {
-
-  if (Serial.available() > 0) {
-  int a = Serial.parseInt();
-  servo1.write(a);
+  if(Serial.available() > 0) {
+    int a = Serial.parseInt();
+    servo1.write(a);
+    Serial.println(a);
   }
 }
 
