47b5c52fa21edcfa7eb90ba2cbc6c600b5ba541a
TheBritKnight <charles.dwsn@gmail.com>
2014-02-26 19:05:16 -0500

    Commits

--- Arduino Stuff/BasicServo/ServoProtocol/ServoProtocol.ino
+++ Arduino Stuff/BasicServo/ServoProtocol/ServoProtocol.ino
@@ -1,27 +1,29 @@
 #include <Servo.h> 
 
 #define servo1Pin 10
-#define servo1
+#define USE_SERVOS
 
 int servoNumberPin[] = {servo1Pin};
-Servo servo[] = {servo1};
 int servoValues[] = {0};
+
+Servo servo1;
 
 String inputData = "";
 
 void setup() {
   initSerial();
   
-  for (int i = 0; i <= 0; i++) {
-    servo[i].attach(servoNumberPin[i]);
-  }  
+  servo1.attach(10);  
+  
+  servo1.write(100);
+  delay(500);
+  servo1.write(90);
+  delay(500);
+  servo1.write(120);
 }
 
 void loop() {
-  recievePacket();
-  parseThePacket();
-//actOnDepthValues();
-  act();
+
 }
 
 void initSerial() {
@@ -29,65 +31,31 @@ void initSerial() {
   Serial.begin(9600);
 }
 
-String recievePacket() {
-  int index = 0;
-  char data[36]; //32 + 4 for 1 servo
+void serialEvent() {
+  while (Serial.available() <= 0 || (char)Serial.read() != '!') {
+    // Wait for start signal...
+  }
+  delay(1); // To give the next byte a chance to come in
   
-  while (Serial.available() <= 0 || Serial.read() != '!') {}
-  delay(1);
+  // By this point we know that either there is nothing in the RX buffer
+  //   or the first byte in the buffer is the second byte of the message
+  //   (as we already read the first byte to see if it was a '!')
+  String inputString = "";
   while (Serial.available() > 0) {
-    char input = Serial.read();
-    if (input == '$') { break; }
-    data[index] = input;
-    index ++;
-    delay(1);
- }
- data[index] = '\0';
- inputData = String(data);
-}
-
-void parseThePacket() {
- // parseMotorValues();
-  parseServoValues();
-
-}
-
-void act() {
-  // Act
-  //actOnMotors();
-  actOnServos();
+    char in_char = (char)Serial.read();
+    
+    if (in_char == '$') {
+      break; // Stop byte recieved, breaking out
+    }
+    
+    inputString += in_char;
+  }
   
-}
-
-void parseServoValues() {
-  for (int i = 1; i <= 1; i++) {
-    servoValues[i-1] = getServoValueAt(i);
+#ifdef USE_SERVOS
+  Serial.println(inputString);
+  for (int i = 0; i < 1; i++) {
+    int s_val = inputString.substring(24 + i*4, 24 + i*4 + 3).toInt();
+    servoValues[i] = s_val;
   }
+#endif
 }
-
-int getServoValueAt(int servoNumber) {
-  int placeHolder = 0;
-  switch(servoNumber) {
-    case 1:
-      placeHolder = 25;
-      break;
-  }
-  String val = inputData.substring(placeHolder, placeHolder+3);
-  return val.toInt();
-}
-
-void actOnServos() {
-  for (int i = 0; i <= 0; i++) {
-    int a = servoValues[i];
-    servo[i].write(a);
-  }
-}
-
-
-
-
-
-
-
-
-  
